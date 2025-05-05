0f80e9048e2babd5ab14c569e2596330f149d4f1
Long Lam <long.lam.1254@gmail.com>
2025-05-05 15:05:50 -0700

    Minor cleanups

--- lib/main.dart
+++ lib/main.dart
@@ -30,13 +30,11 @@ Future<void> _initFirebaseMessaging() async {
   // Request permission
   await messaging.requestPermission();
 
-
   await messaging.subscribeToTopic('daily_reminder');
 
   // Testing purposes
   String? token = await messaging.getToken();
   print('FCM Token: $token');
-
 
   // Listen for foreground messages, when the app is open it should display a pop-up message on top
   FirebaseMessaging.onMessage.listen((RemoteMessage message) {
