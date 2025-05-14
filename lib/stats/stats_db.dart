80749b9d4dbebed42603a048beb393e4bf29abfd
Luke Walker <lukeryanwalker2000@gmail.com>
2025-05-13 20:44:49 -0700

    Fix database schema inconsistency

--- lib/stats/stats_db.dart
+++ lib/stats/stats_db.dart
@@ -19,61 +19,69 @@ class StatsDatabase {
     return _db!;
   }
 
+  Future<void> _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE IF NOT EXISTS exercise (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        type TEXT UNIQUE NOT NULL
+      )''');
+    await db.execute('''
+      CREATE TABLE IF NOT EXISTS answer (
+        exercise_id INTEGER,
+        name TEXT,
+        correct INTEGER NOT NULL DEFAULT 0,
+        incorrect INTEGER NOT NULL DEFAULT 0,
+        FOREIGN KEY (exercise_id) REFERENCES exercise (id),
+        PRIMARY KEY (exercise_id, name)
+      )''');
+    await db.execute('''
+      CREATE TABLE IF NOT EXISTS module (
+        exercise_id INTEGER,
+        name TEXT,
+        high_score INTEGER NOT NULL DEFAULT 0,
+        times_completed INTEGER NOT NULL DEFAULT 0,
+        FOREIGN KEY (exercise_id) REFERENCES exercise (id),
+        PRIMARY KEY (exercise_id, name)
+      )''');
+    await db.execute('''
+      CREATE TABLE IF NOT EXISTS daily (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        date TEXT UNIQUE,
+        practice_time INTEGER NOT NULL DEFAULT 0
+      )''');
+    await db.execute('''
+      CREATE TABLE IF NOT EXISTS exercise_score (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        exercise_id INTEGER NOT NULL,
+        daily_id INTEGER NOT NULL,
+        score INTEGER NOT NULL,
+        max_score INTEGER NOT NULL,
+        bg_noise REAL NOT NULL DEFAULT 0,
+        FOREIGN KEY (exercise_id) REFERENCES exercise (id),
+        FOREIGN KEY (daily_id) REFERENCES daily (id)
+      )''');
+
+    // Fill exercise table
+    const exerciseTypes = ["words", "sounds", "speech", "music"];
+    for (final type in exerciseTypes) {
+      Exercise.insert(Exercise(type: type));
+    }
+  }
+
+  Future<void> _onUpgrade(Database db, int oldVersion, int newVersion) async {
+    if (oldVersion < 2 && newVersion >= 2) {
+      await db.execute("DROP TABLE exercise");
+      await _onCreate(db, newVersion);
+    }
+  }
+
   Future<Database> _initDatabase() async {
     final path = join(await getDatabasesPath(), "stats.db");
     return await openDatabase(
-      path,
-      version: 1,
-      onCreate: (db, version) async {
-        // Create tables
-        await db.execute('''
-          CREATE TABLE IF NOT EXISTS exercise (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            type TEXT UNIQUE NOT NULL
-          )''');
-        await db.execute('''
-          CREATE TABLE IF NOT EXISTS answer (
-            exercise_id INTEGER,
-            name TEXT,
-            correct INTEGER NOT NULL DEFAULT 0,
-            incorrect INTEGER NOT NULL DEFAULT 0,
-            FOREIGN KEY (exercise_id) REFERENCES exercise (id),
-            PRIMARY KEY (exercise_id, name)
-          )''');
-        await db.execute('''
-          CREATE TABLE IF NOT EXISTS module (
-            exercise_id INTEGER,
-            name TEXT,
-            high_score INTEGER NOT NULL DEFAULT 0,
-            times_completed INTEGER NOT NULL DEFAULT 0,
-            FOREIGN KEY (exercise_id) REFERENCES exercise (id),
-            PRIMARY KEY (exercise_id, name)
-          )''');
-        await db.execute('''
-          CREATE TABLE IF NOT EXISTS daily (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            date TEXT UNIQUE,
-            practice_time INTEGER NOT NULL DEFAULT 0
-          )''');
-        await db.execute('''
-          CREATE TABLE IF NOT EXISTS exercise_score (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            exercise_id INTEGER NOT NULL,
-            daily_id INTEGER NOT NULL,
-            score INTEGER NOT NULL,
-            max_score INTEGER NOT NULL,
-            bg_noise REAL NOT NULL DEFAULT 0,
-            FOREIGN KEY (exercise_id) REFERENCES exercise (id),
-            FOREIGN KEY (daily_id) REFERENCES daily (id)
-          )''');
-
-        // Fill exercise table
-        const exerciseTypes = [
-          "words", "sounds", "speech", "music"];
-        for (final type in exerciseTypes) {
-          Exercise.insert(Exercise(type: type));
-        }
-      }
+        path,
+        version: 2,
+        onCreate: _onCreate,
+        onUpgrade: _onUpgrade
     );
   }
 }
